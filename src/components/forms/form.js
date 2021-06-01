94418577bf79136971c8b234851a90a1d8b7c6e5
Admin <jayarex07@gmail.com>
2021-06-01 16:02:20 -0400

    "Initial Contact - added multi-step forms"

---
+++ src/components/forms/form.js
@@ -0,0 +1,172 @@
+import React from "react"
+import "../styles/style.scss"
+
+export default function Form () {
+
+    return(
+<>
+<form name="contact v3"
+method="post" 
+netlify-honeypot="bot-field" 
+data-netlify="true" 
+onSubmit="submit">
+
+
+<section className="section is-large">
+<div className="columns is-centered">
+    <div className="column is-6 ">
+<div className="field">
+    <label className="label">Name</label>
+    <div className="control">
+    <input type="hidden" name="form-name" value="contact v3" />
+    <input type="hidden" name="bot-field" />
+
+        <input className="input" type="text" name="Name/Business" placeHolder="Text input" />
+    </div>
+</div>
+
+<div className="field">
+    <label className="label">Email</label>
+    <div className="control">
+        <input className="input" type="email" placeHolder="Email input" name="Email" />
+            <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+            </span> 
+    </div>
+</div>
+
+<div className="field">
+    <label className="label">Subject</label>
+    <div className="control">
+        <div className="select">
+            <select name="option">
+                <option>Option #1</option>
+                <option>Option #2</option>
+            </select>
+        </div>
+    </div>
+</div>
+
+<div className="field">
+    <label className="label">Message</label>
+    <div className="control">
+        <textarea className="textarea" name="Message" placeHolder="Textarea"></textarea>
+    </div>
+</div>
+
+<div className="field is-grouped is-grouped-centered">
+    <div className="control">
+       <button className="button is-link">Submit</button>
+    </div>
+    <div className="control">
+       <button className="button is-link is-light">Cancel</button>
+    </div>
+</div>
+</div>
+</div>
+</section>
+</form>
+</>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+import React, { Component } from 'react';
+import FormUserDetails from './user-details';
+import FormPersonalDetails from './personal-details';
+import Confirm from './confirm';
+import Success from './success';
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
+    bio: ''
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to prev step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+
+
+
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+
+            <form name="contact v3"
+method="post" 
+netlify-honeypot="bot-field" 
+data-netlify="true" 
+onSubmit="submit">
+<>
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          /> </> </form>
+        );
+      case 4:
+        return <Success />;
+      default:
+        (console.log('This is a multi-step form built with React.'))
+
+        
+    }
+  }
+}
+
+export default UserForm;
