acfef6ff2bca9767fa45c60ea4762db4ae63c181
Admin <jayarex07@gmail.com>
2021-07-25 09:53:48 -0400

    Preparing for Lift Off (small changes to landing page)

--- src/components/home/contact.js
+++ src/components/home/contact.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Fade from 'react-reveal/Fade'
-import { Link } from "gatsby"
 
 
 
@@ -8,16 +7,7 @@ export default function InitializeContact() {
     return (
 
       <>
-    <div className=" orange bold has-text-centered" style={{marginTop: 24.5 + "rem"}}>
 
-    <div className="">
-        <Fade cascade duration={2000} delay={800}><span>Feel Free To Explore Around</span></Fade> <br/>
-        <Fade cascade duration={1500} delay={1400}><span style={{fontSize: 2 + 'rem'}}>And When Ready, <span>
-                    <div className="button resume-link"><Link to="/contact">Intialize Contact!</Link></div></span></span></Fade><br/><br/>
-        <Fade cascade duration={2000} delay={2300}><span>Safe Travels!</span></Fade><br/>
-        <Fade cascade duration={2000} delay={3000}><span>As I Look Forward To Working Together.</span></Fade><br/>
-      </div>
-</div>
 </>
     )
 }
