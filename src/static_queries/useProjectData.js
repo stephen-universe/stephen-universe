72ffecafbc9085abd5c682854167d37f74e6c3d8
jayareX7 <jayarex07@gmail.com>
2025-04-30 02:36:54 -0500

    testing build

---
+++ src/static_queries/useProjectData.js
@@ -0,0 +1,25 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+export default function useProjectData() {
+  const data = useStaticQuery(graphql`
+    query getProjectData {
+      allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              date(formatString: "MMMM Do, YYYY")
+              author
+              title
+            }
+            excerpt(pruneLength: 200)
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  return data.allMarkdownRemark.edges
+}
