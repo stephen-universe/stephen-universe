288f3e273b20603087d3ec3d40830c0a48ff2686
jayareX7 <jayarex07@gmail.com>
2025-05-05 16:58:40 -0500

    added about section to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,40 @@
 
-import React from "react"
-import Form from "../components/forms/form"
-import Layouts from "../components/Layouts"
+import React from "react";
+import Helmet from "react-helmet";
+import About from "../components/home/about";
+import Layouts from "../components/Layouts";
 
-export default function Contact() {
 
-
+export default function ProjectPage() {
+  // Little helpers ...
+ 
 
   return (
-    <>
-<section className="main-body-bg">
-<Layouts>
+   <Layouts>
+      <> 
+        
+          <div className="background">
+            <div className="section-divider"></div>
+            <div className="container">
+              <Helmet>
+                <html lang="en" />
+                <title>Stephen-Universe</title>
+                <meta name="description" content="Portfolio" />
+              </Helmet>
 
-  
-         
-  <section className="section">
-      <form name="Contact Form v1"
-          method="post" 
-          netlify-honeypot="bot-field" 
-          data-netlify="true" 
-          onSubmit="submit"
-          > 
-        <div className="tile is-ancestor">
-          <div className="tile is-12">
-            <div className="tile">
-              <div className="tile is-parent">
-                <div className="tile is-child box">
-                  
-                  <Form />
-                </div>
-              </div>
+             
             </div>
           </div>
-        </div>
-      </form>
-  </section>
-</Layouts>
-</section>
-    </>
-  )
+          
+    
+     
+        
+     
+        
+        <div className="body-bg">
+         <About />
+         </div>   
+         </>
+ </Layouts>
+  );
 }
