bfda972b2ba96abd95e544ae7083c572e3c866c9
jayareX7 <jayarex07@gmail.com>
2025-05-06 10:00:04 -0500

    updated Tag ID’s

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -24,60 +24,57 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       <script
         key="fb-pixel"
         dangerouslySetInnerHTML={{
-          __html: `            
-            !function(f,b,e,v,n,t,s) { 
-              if(f.fbq)return; 
-              n=f.fbq=function(){n.callMethod? 
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)}; 
-              if(!f._fbq)f._fbq=n; 
-              n.push=n; 
-              n.loaded=!0; 
-              n.version='2.0'; 
-              n.queue=[]; 
-              t=b.createElement(e); 
-              t.async=!0; 
-              t.src=v; 
-              s=b.getElementsByTagName(e)[0]; 
-              s.parentNode.insertBefore(t,s)
-            }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); 
-            fbq('init', '455867946463533'); 
-            fbq('track', 'PageView');`,
+          __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '582586296321978');
+            fbq('track', 'PageView');
+            `,
         }}
       />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style="display:none"
+          src="https://www.facebook.com/tr?id=582586296321978&ev=PageView&noscript=1"
+        />
+      </noscript>
       <script
         key="twitter-pixel"
         dangerouslySetInnerHTML={{
-          __html: `!function(e,t,n,s,u,a){ 
-            e.twq||(s=e.twq=function(){
-              s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-            }, 
-            s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,
-            u.src='//static.ads-twitter.com/uwt.js',
-            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))
-          }(window,document,'script');
-          twq('init','porr8');
-          twq('track','PageView');`,
+          __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','porr8');`,
         }}
       />
+
       {/* Microsoft Clarity */}
       <script
         key="clarity"
         dangerouslySetInnerHTML={{
-          __html: `(function(c,l,a,r,i,t,y){ 
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; 
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; 
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); 
-          })(window, document, "clarity", "script", "8354993");`,
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "rf1j4qelk0");`,
         }}
       />
       {/* LinkedIn Insight Tag */}
       <script
         key="linkedin"
         dangerouslySetInnerHTML={{
-          __html: `
-            _linkedin_partner_id = "8354993";
-            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "rf1j4qelk0");
           `,
         }}
       />
@@ -85,24 +82,19 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
         key="linkedin-loader"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(){
-              var s = document.getElementsByTagName("script")[0];
-              var b = document.createElement("script");
-              b.type = "text/javascript";b.async = true;
-              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-              s.parentNode.insertBefore(b, s);
-            })();
+            (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk); 
           `,
         }}
       />
       <noscript>
+        {" "}
         <img
           height="1"
           width="1"
-          style={{ display: "none" }}
+          style="display:none;"
           alt=""
           src="https://px.ads.linkedin.com/collect/?pid=8354993&fmt=gif"
-        />
+        />{" "}
       </noscript>
     </>
   );
@@ -128,9 +120,9 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
         function loadTrackers() {
           const fragment = document.createRange().createContextualFragment(\`
             ${trackingScripts.props.children
-              .filter(script => script.props?.dangerouslySetInnerHTML)
-              .map(script => script.props.dangerouslySetInnerHTML.__html)
-              .join('')}
+              .filter((script) => script.props?.dangerouslySetInnerHTML)
+              .map((script) => script.props.dangerouslySetInnerHTML.__html)
+              .join("")}
           \`);
           document.body.appendChild(fragment);
         }
