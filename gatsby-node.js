b22f3214e04cef0a18290c65d239480ec4efb4cb
jayareX7 <github@stephen-universe.com>
2025-05-29 19:55:21 -0500

    changed object keys location

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,34 @@
 const path = require("path")
-
-
-const projectDetails = require("./content/data/projects.json");
+const projectDetails = require("./content/data/projects.json")
 
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions;
 
-  Object.keys(projectDetails).forEach((slug) => {
+  // Access the projects object from your JSON structure
+  Object.keys(projectDetails.projects).forEach((slug) => {
     createPage({
       path: `/project/${slug}`,
       component: path.resolve("./src/templates/project.js"),
-      context: { slug },
+      context: { 
+        slug,
+        // You can also pass the entire project data if needed
+        projectData: projectDetails.projects[slug]
+      },
     });
   });
 };
 
-
 module.exports.onCreateNode = ({ node, actions }) => {
-  // Transform the new node here and create a new node or
-  // create a new node field.
   const { createNodeField } = actions
   if (node.internal.type === "MarkdownRemark") {
     const slug = path.basename(node.fileAbsolutePath, ".md")
     createNodeField({
-      //same as node: node
       node,
       name: "slug",
       value: slug,
     })
   }
 }
-
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
@@ -44,4 +42,3 @@ exports.createSchemaCustomization = ({ actions }) => {
   `
   createTypes(typeDefs)
 }
-
