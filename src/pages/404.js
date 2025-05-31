4542457cb8413b88f85653992dcbc6f348bc7734
jayareX7 <github@stephen-universe.com>
2025-05-31 02:33:56 -0500

    updated import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
-import Layouts from "../../components/layouts"
+import dynamic from 'next/dynamic';
+
+const Layouts = dynamic(() => import("../../components/layouts"), { ssr: false });
 
 
 export default function NotFound() {
