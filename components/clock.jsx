83404710eb69ab23feacc8331367fc8771b1e0e7
jayareX7 <github@stephen-universe.com>
2025-05-31 01:58:55 -0500

    added a separate time component

---
+++ components/clock.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+
+function getCurrentTime() {
+  const now = new Date();
+  return `${String(now.getHours()).padStart(2, "0")}:${String(
+    now.getMinutes()
+  ).padStart(2, "0")}:${String(now.getSeconds()).padStart(2, "0")}`;
+}
+
+export default function Clock() {
+  const [time, setTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getCurrentTime());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="text-animation-clock">
+      <i className="fa-solid fa-earth-americas"></i> {time}
+    </div>
+  );
+}
