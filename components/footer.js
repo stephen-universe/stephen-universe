6e60c5b1b740cf9a6b6eec3218fe8bd8a0744dd3
jayareX7 <github@stephen-universe.com>
2025-06-02 04:40:01 -0500

    added footer links

--- components/footer.js
+++ components/footer.js
@@ -3,31 +3,37 @@ import Link from "next/link";
 import appData from "../content/data/navigation.json";
 
 export default function Footer() {
+  const { footerLinks, footerText } = appData;
+
   return (
     <>
       <div className="section-divider"></div> 
       <footer className="footer-bg" aria-label="Space"></footer>
 
       <div className="footer-bottom-color">
-        <div className="footer-bottom-color container">
-          <div className='react-made'>
-            {appData.footerText[0].text1} 
-            {appData.footerLinks.map((link, index) => (
-              <React.Fragment key={index}>
-                {' '}
-                <Link href={link.url} className={link.className}>
-                  {link.text}
-                </Link>
-              </React.Fragment>
-            ))}
+          <div className="react-made">
+            {footerText[0].text1}
+            {' '}
+            <Link data-label="clicked on link to React" href={footerLinks.react.url} className={footerLinks.react.className}>
+              {footerLinks.react.text}
+            </Link>
+            {'|'}
+            <Link data-label="clicked on link to Next.js" href={footerLinks.nextjs.url} className={footerLinks.nextjs.className}>
+              {footerLinks.nextjs.text}
+            </Link>
+            {'|'}
+            <Link data-label="clicked on link to Framer Motion" href={footerLinks.framer.url} className={footerLinks.framer.className}>
+              {footerLinks.framer.text}
+            </Link>
+            {'|'}
+            <Link data-label="clicked on link to TinaCMS" href={footerLinks.tina.url} className={footerLinks.tina.className}>
+              {footerLinks.tina.text}
+            </Link>
+          </div>
+          <div className="reserved-rights">
+            {footerText[0].text2}
           </div>
         </div>
-        <div className="footer-bottom-color">
-          <div className='footer-bottom-color reserved-rights'>
-            {appData.footerText[0].text2}
-          </div>
-        </div>
-      </div>
     </>
   );
 }
