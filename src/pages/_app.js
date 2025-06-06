39d339b887093080eae7d1b29d9e73f16e3bc696
jayareX7 <github@stephen-universe.com>
2025-06-05 19:29:13 -0500

    updated clarity tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -92,13 +92,16 @@ function MyApp({ Component, pageProps }) {
 
       if (hasConsent("analytics") && CLARITY_ID) {
         loadScript({
-          textContent: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "${CLARITY_ID}");`,
+          textContent: `
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/${CLARITY_ID}";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "${CLARITY_ID}");
+    `,
         });
       }
+
 
       // No-script fallback
       setTimeout(() => {
